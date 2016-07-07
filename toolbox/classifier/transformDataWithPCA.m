68290119687e0c011b7718e51b42b59167a4a4ad
David Brainard <brainard@psych.upenn.edu>
2016-07-07 17:00:42 -0400

    Little stuff
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- toolbox/classifier/transformDataWithPCA.m
+++ toolbox/classifier/transformDataWithPCA.m
@@ -1,21 +1,22 @@
-function data = transformDataWithPCA(data,numPCA,varargin)
+function data = transformDataWithPCA(data,numPCAComponents,varargin)
 % data = transformDataWithPCA(data,numPCA,varargin)
 %
 % Projects data along a specified number of principal components. PCA is
 % done on the data matrix and the first numPCA components (ordered by
 % variance explained) will be used to project the data into a lower
-% dimension. By default, the data will be standardized before the PCA
-% calculation.
+% dimensional space.
+%
+% By default, the data will be standardized before the PCA calculation.
 % 
 % Inputs:
-%   data    -  A matrix containing data to project into a lower dimension.
-%              Rows represent instances of data and columns are features.
+%   data             -  A matrix containing data to project into a lower dimension.
+%                       Rows represent instances of data and columns are features.
 %
-%   numPCA  -  The number of principal components to project onto.
+%   numPCAComponents -  The number of principal components to project onto.
 %
-%  {Optional Paramter}
-%   toStd   -  A boolean to determine whether or not to standardize the
-%              data. By default this is set to true.
+% Optional Paramters as key value paris
+%   'Standardize'    -  true/false: determine whether or not to standardize the
+%                       data, default true.
 %
 % 7/7/16  xd  wrote it
 
@@ -23,20 +24,19 @@ function data = transformDataWithPCA(data,numPCA,varargin)
 p = inputParser;
 p.addRequired('data',@isnumeric);
 p.addRequired('numPCA',@isnumeric);
-p.addParameter('toStd',true,@islogical);
-
-p.parse(data,numPCA,varargin{:});
-toStd = p.Results.toStd;
+p.addParameter('Standardize',true,@islogical);
+p.parse(data,numPCAComponents,varargin{:});
+STANDARDIZE = p.Results.Standardize;
 
 %% Standardize the data
-if toStd
+if (STANDARDIZE)
     m = mean(data,1);
     s = std(data,1);
     data = (data - repmat(m,size(data,1),1)) ./ repmat(s,size(data,1),1);
 end
 
-%% Do PCA and projects data into new vector space
-coeff = pca(data,'NumComponents',numPCA);
+%% Do PCA and project data into new vector space
+coeff = pca(data,'NumComponents',numPCAComponents);
 data = data*coeff;
 
 end
