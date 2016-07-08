31d3f60b5528a2d2259890f62481de2817aae98f
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-07-08 10:11:06 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- tutorials/colorGaborCRTStimulusCreate.m
+++ tutorials/colorGaborCRTStimulusCreate.m
@@ -11,68 +11,60 @@ function colorGaborCRTStimulusCreate
     
     staticScene = colorGaborSceneCreate(p, coneContrasts, backgroundxyY, monitorFile, viewingDistanceInMeters);
     vcAddObject(staticScene); sceneWindow;
-    staticPhotons = sceneGet(staticScene, 'photons');
 
     % light leakage image (10.0 cd/m2)
     leakagexyY = [0.33 0.33 10.0]';
     leakageScene = colorGaborSceneCreate(p, [0 0 0]', leakagexyY, monitorFile, viewingDistanceInMeters);
-    leakagePhotons = sceneGet(leakageScene, 'photons');
-    
     
     % Generate phosphor activation function (fast onset, slower decay)
     refreshRate = 60;
     phosphorFunction = generatePhosphorActivationFunction(refreshRate);
-        
+         
     % Stimulus temporal ramping params
-    rampTauInSeconds = 0.165;  stimulusDurationInSeconds = 5*rampTauInSeconds;
+    rampTauInSeconds = 0.165;  stimulusDurationInSeconds = 5.5*rampTauInSeconds;
     
-    % Generate stimulus time-series
-    [rasterPhotons, timeInSeconds, rasterActivation] = generateRasterSequence(staticPhotons, phosphorFunction, stimulusDurationInSeconds);
-    
-    % Ramp stimulus presentation
-    [rasterPhotons, temporalEnvelope] = generateRampedSequence(rasterPhotons, leakagePhotons, timeInSeconds, rampTauInSeconds);
-    
-    % Visualize
-    visualizeStimulusTimeSeries(timeInSeconds,rasterPhotons, rasterActivation, temporalEnvelope);
-end
-
-function [rasterPhotons, timeInSeconds, rasterActivation] = generateRasterSequence(staticPhotons, phosphorFunction, stimulusDurationInSeconds)
+    % Generate frame scenes
     dt = phosphorFunction.timeInSeconds(2)-phosphorFunction.timeInSeconds(1);
     tBins = round(stimulusDurationInSeconds/dt);
     rasterActivation = [];
+    frameIndex = 0;
+    to = stimulusDurationInSeconds/2;
+    totalFrames = round(stimulusDurationInSeconds/phosphorFunction.timeInSeconds(end));
     while numel(rasterActivation) < tBins
+        fprintf('Generating frame %d of %d\n', frameIndex, totalFrames)
+        frameTimeInSeconds = frameIndex * phosphorFunction.timeInSeconds(end);
+        frameIndex = frameIndex+1;
         rasterActivation = cat(2, rasterActivation, phosphorFunction.activation);
+        rampGain = exp(-((frameTimeInSeconds-to)/rampTauInSeconds).^2);
+        frameSequence(frameIndex) = struct(...
+            'scene', colorGaborSceneCreate(p, coneContrasts*rampGain, backgroundxyY, monitorFile, viewingDistanceInMeters),...
+            'leakageScene', leakageScene, ...
+            'timeInSeconds', frameTimeInSeconds, ...
+            'rampGain', rampGain, ...
+            'phosphorFunction', phosphorFunction);
     end
-    rasterActivation = rasterActivation(1:tBins);
-    tBins = numel(rasterActivation);
-    timeInSeconds = (0:(tBins-1))*dt;
-    rasterPhotons = bsxfun(@times, staticPhotons, reshape(rasterActivation, [1 1 1 numel(rasterActivation)]));
-end
-
-function [photonSequence, temporalEnvelope] = generateRampedSequence(rasterPhotons, leakagePhotons, timeInSeconds, rampTauInSeconds)
-    t0 = timeInSeconds(1) + (timeInSeconds(end)-timeInSeconds(1))/2;
-    temporalEnvelope = exp(-((timeInSeconds-t0)/rampTauInSeconds).^2);
-    rasterPhotons  = bsxfun(@times, rasterPhotons, reshape(temporalEnvelope, [1 1 1 numel(temporalEnvelope)]));
-    photonSequence = bsxfun(@plus, rasterPhotons, leakagePhotons);
-end
-
-function phosphorFunction = generatePhosphorActivationFunction(refreshRate) 
-    alpha = 1.9; t_50 = 0.05/1000; n = 2;
-    samplesPerRefreshCycle = 33;
-    phosphorFunction.timeInSeconds = linspace(0,1.0/refreshRate,samplesPerRefreshCycle);
-    phosphorFunction.activation = (phosphorFunction.timeInSeconds.^n)./(phosphorFunction.timeInSeconds.^(alpha*n) + t_50^(alpha*n));
-    phosphorFunction.activation = phosphorFunction.activation - phosphorFunction.activation(end);
-    phosphorFunction.activation(phosphorFunction.activation<0) = 0;
-    phosphorFunction.activation = phosphorFunction.activation / max(phosphorFunction.activation);
-end
-
-function visualizeStimulusTimeSeries(timeInSeconds,rasterPhotons, rasterActivation, temporalEnvelope)  
     
-    totalEnergyTimeSeries = squeeze(sum(rasterPhotons,3));
-    maxEnergy = max(totalEnergyTimeSeries(:));
-    minEnergy = min(totalEnergyTimeSeries(:));
+    visualizeStimulus(frameSequence);
+end
+
+function visualizeStimulus(frameSequence)
+    framesNum = numel(frameSequence);
+    leakagePhotons = sceneGet(frameSequence(1).leakageScene, 'photons');
     
-    hFig = figure(1); clf; colormap(gray(1024));
+    for iFrame = 1:framesNum
+        framePhotons = sceneGet(frameSequence(iFrame).scene, 'photons');
+        framePhotons = framePhotons + leakagePhotons;
+        if (iFrame == 1)
+            totalEnergy = zeros(size(framePhotons,1), size(framePhotons,2), framesNum);
+        end
+        % sum photons over all waveleghts
+        totalEnergy(:,:, iFrame) = squeeze(sum(framePhotons,3));
+    end
+    
+    maxEnergy = max(totalEnergy(:));
+    minEnergy = min(totalEnergy(:));
+    
+    hFig = figure(1); clf; 
     set(hFig, 'Position', [10 10 1520 960], 'Color', [1 1 1]);
     
     % Open video stream
@@ -82,23 +74,14 @@ function visualizeStimulusTimeSeries(timeInSeconds,rasterPhotons, rasterActivati
     writerObj.Quality = 100;
     writerObj.open();
     
-    for tBin = 1:size(rasterPhotons,4)
-        subplot('Position', [0.03 0.62 0.95 0.35]);
-        plot(timeInSeconds*1000, rasterActivation, 'k.-', 'Color', [0.5 0.5 0.5], 'LineWidth', 2.0);
-        hold on;
-        plot(timeInSeconds*1000, temporalEnvelope, 'r.-', 'LineWidth', 2.0);
-        plot(timeInSeconds(tBin)*1000*[1 1], [-0.5 1.5], '-', 'Color', [0 0.3 1.0 0.5], 'LineWidth', 3.0);
-        plot(timeInSeconds(tBin)*1000, rasterActivation(tBin), 'ko', 'MarkerSize', 12, 'MarkerFaceColor', [1.0 0.5 0.5], 'MarkerEdgeColor', [1 0.2 0.2], 'LineWidth', 1.0);
-        yTicks =  0:0.2:1.0;
-        set(gca, 'XLim', [timeInSeconds(1) timeInSeconds(end)]*1000, 'YLim', [-0.01 1.01], 'YTick', yTicks, 'YTickLabel', sprintf('%-1.1f\n', yTicks), 'FontSize', 14);
-        xlabel('time (ms)', 'FontWeight', 'bold', 'FontSize', 16);
-        hL = legend({'raster', 'ramp'});
-        set(hL, 'FontSize', 12);
-        hold off; 
-        box off
-        
+    rasterTrace = []; rampTrace = []; rampTime = [];
+    totalTimeInSeconds = frameSequence(framesNum).timeInSeconds(end);
+    
+    for iFrame = 1:framesNum
+        phosphorFunction = frameSequence(iFrame).phosphorFunction;
+            
         subplot('Position', [0.01 0.02 0.49 0.49]);
-        energyFrame = squeeze(totalEnergyTimeSeries(:,:,tBin));
+        energyFrame = squeeze(totalEnergy(:,:,iFrame));
         imagesc(energyFrame);
         set(gca, 'XTick', [], 'YTick', []);
         axis 'image'
@@ -109,26 +92,56 @@ function visualizeStimulusTimeSeries(timeInSeconds,rasterPhotons, rasterActivati
         hCbar.FontSize = 16; 
         hCbar.FontName = 'Menlo'; 
         hCbar.Color = [0.2 0.2 0.2];
-        title(sprintf('CRT photon emission map (t: %2.3f ms)', 1000*timeInSeconds(tBin)), 'FontSize', 14);
+        title(sprintf('CRT photon emission map (frame: %d)', iFrame), 'FontSize', 14);
+
+        for rasterBin = 1:numel(phosphorFunction.timeInSeconds)
+            subplot('Position', [0.51 0.02 0.49 0.49]);
+            rasterFrame = energyFrame * phosphorFunction.activation(rasterBin);
+            imagesc(rasterFrame);
+            set(gca, 'XTick', [], 'YTick', []);
+            axis 'image'
+            set(gca, 'CLim', [minEnergy maxEnergy]);
+            hCbar = colorbar(); % 'Ticks', cbarStruct.ticks, 'TickLabels', cbarStruct.tickLabels);
+            hCbar.Orientation = 'vertical'; 
+            hCbar.Label.String = 'CRT photon emission rate (photons/sec)'; 
+            hCbar.FontSize = 16; 
+            hCbar.FontName = 'Menlo'; 
+            hCbar.Color = [0.2 0.2 0.2];
+            title(sprintf('CRT raster photon emission map (t: %2.2fms)', 1000*(frameSequence(iFrame).timeInSeconds+phosphorFunction.timeInSeconds(rasterBin))), 'FontSize', 14);
+
+            rasterTrace = cat(2,rasterTrace, phosphorFunction.activation(rasterBin));
+            rasterTime  = (1:numel(rasterTrace))*(phosphorFunction.timeInSeconds(2)-phosphorFunction.timeInSeconds(1));
+            rampTrace = cat(2, rampTrace, frameSequence(iFrame).rampGain);
+            
+            subplot('Position', [0.03 0.62 0.95 0.35]);
+            plot(rasterTime*1000, rasterTrace, 'k.-', 'Color', [0.5 0.5 0.5], 'LineWidth', 2.0);
+            hold on
+            stairs(rasterTime*1000, rampTrace, 'Color', [1 0 0], 'LineWidth', 3.0);
+            hold off;
+            yTicks =  0:0.2:1.0;
+            set(gca, 'XLim', [0 totalTimeInSeconds]*1000, 'YLim', [-0.01 1.01], 'YTick', yTicks, 'YTickLabel', sprintf('%-1.1f\n', yTicks), 'FontSize', 14);
+            xlabel('time (ms)', 'FontWeight', 'bold', 'FontSize', 16);
+            hL = legend({'CRT raster', 'contrast ramp'});
+            set(hL, 'FontSize', 12);
+            box off
         
-        subplot('Position', [0.51 0.02 0.49 0.49]);
-        imagesc(energyFrame*0);
-        set(gca, 'XTick', [], 'YTick', []);
-        axis 'image'
-        set(gca, 'CLim', [0 1]);
-        hCbar = colorbar(); % 'Ticks', cbarStruct.ticks, 'TickLabels', cbarStruct.tickLabels);
-        hCbar.Orientation = 'vertical'; 
-        hCbar.Label.String = 'Isomerization map (R*/cone/sec)'; 
-        hCbar.FontSize = 16; 
-        hCbar.FontName = 'Menlo'; 
-        hCbar.Color = [0.2 0.2 0.2];
-        title(sprintf('cone isomerization map (t: %2.3f ms)', 1000*timeInSeconds(tBin)), 'FontSize', 14);
-         
-        % Write video frame
-        drawnow;
-        writerObj.writeVideo(getframe(hFig));
+            % Write video frame
+            colormap(gray(1024));
+            drawnow;
+            writerObj.writeVideo(getframe(hFig));
+        end % rasterBin
     end
-    
     % Close video stream
     writerObj.close();
+end
+
+
+function phosphorFunction = generatePhosphorActivationFunction(refreshRate) 
+    alpha = 1.9; t_50 = 0.05/1000; n = 2;
+    samplesPerRefreshCycle = round(2*1000/refreshRate)
+    phosphorFunction.timeInSeconds = linspace(0,1.0/refreshRate, samplesPerRefreshCycle);
+    phosphorFunction.activation = (phosphorFunction.timeInSeconds.^n)./(phosphorFunction.timeInSeconds.^(alpha*n) + t_50^(alpha*n));
+    phosphorFunction.activation = phosphorFunction.activation - phosphorFunction.activation(end);
+    phosphorFunction.activation(phosphorFunction.activation<0) = 0;
+    phosphorFunction.activation = phosphorFunction.activation / max(phosphorFunction.activation);
 end
