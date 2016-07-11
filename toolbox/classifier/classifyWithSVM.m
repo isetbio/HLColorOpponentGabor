6c31f906074a61965e7f87f493860d4762b424cc
Xiaomao Ding <xiaomaoding@seal.psych.upenn.edu>
2016-07-11 17:48:04 -0400

    return stdErr in classifyWithSVM

--- toolbox/classifier/classifyWithSVM.m
+++ toolbox/classifier/classifyWithSVM.m
@@ -1,10 +1,10 @@
-function percentCorrect = classifyWithSVM(data,classes)
+function [percentCorrect,stdErr] = classifyWithSVM(data,classes)
 % percentCorrect = classifyWithSVM(data,classes)
 % 
 % Trains a SVM using 10-kFold Cross Validation and returns the average
 % percent correct. The data will be divided into ten roughly evenly sized
 % sets. For each set, the SVM will be trained on the other nine and tested
-% on set held out. Then, the average percent correct is returned.
+% on the set held out. Then, the average percent correct is returned.
 %
 % Inputs:
 %   data     -   A matrix containing entries of data along the rows and
@@ -25,7 +25,9 @@ p.parse(data,classes);
 
 %% Train cross validated SVM
 svm = fitcsvm(data,classes,'KernelScale','auto','CrossVal','on','KFold',10);
-percentCorrect = 1 - kfoldLoss(svm,'lossfun','classiferror');
+percentCorrect = 1 - kfoldLoss(svm,'lossfun','classiferror','mode','individual');
+stdErr = std(percentCorrect)/sqrt(10);
+percentCorrect = mean(percentCorrect);
 
 end
 
