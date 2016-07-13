86b457d71adbd7d98dcdd873d13d7529bf75f9a3
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-07-13 13:15:30 -0400

    visualizeSceneOrOpticalImageSequence now has showLuminanceMap flag
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- toolbox/visualization/visualizeSceneOrOpticalImageSequence.m
+++ toolbox/visualization/visualizeSceneOrOpticalImageSequence.m
@@ -1,4 +1,4 @@
-function visualizeSceneOrOpticalImageSequence(sceneOrOpticalImage, sceneOrOpticalImageSequence,timeAxis,  movieFileName)
+function visualizeSceneOrOpticalImageSequence(sceneOrOpticalImage, sceneOrOpticalImageSequence, timeAxis, showLuminanceMap, movieFileName)
 % visualizeSceneOrOpticalImageSequence(sceneOrOpticalImage, sceneOrOpticalImageSequence, movieFileName)
 %
 % Visualize a sequence of scenes or optical images and render a video of it.
@@ -6,31 +6,35 @@ function visualizeSceneOrOpticalImageSequence(sceneOrOpticalImage, sceneOrOptica
 %
 %  7/12/16  npc Wrote it.
 %
-
-    hFig = figure(10); clf;
-    set(hFig, 'Position', [10 10 950 475], 'Color', [1 1 1]);
-    colormap(hot(1024));
     
     % Loop over the sequence frames and extract the needed info
     for iFrame = 1:numel(sceneOrOpticalImageSequence)
         % Get an RGB rendition of the scene or the optical image
         if (strcmp(sceneOrOpticalImage, 'scene'))
             rgbFrame = sceneGet(sceneOrOpticalImageSequence{iFrame}, 'rgb image');
-            luminanceFrame = sceneGet(sceneOrOpticalImageSequence{iFrame}, 'luminance');
+            if (showLuminanceMap)
+                luminanceFrame = sceneGet(sceneOrOpticalImageSequence{iFrame}, 'luminance');
+            end
             if (iFrame == 1)
                 spatialSupport = sceneGet(sceneOrOpticalImageSequence{iFrame},'spatialsupport','cm');
                 rgbImageSequence = zeros([size(rgbFrame) numel(sceneOrOpticalImageSequence)]);
-                luminanceImageSequence = zeros([size(luminanceFrame) numel(sceneOrOpticalImageSequence)]);
+                if (showLuminanceMap)
+                    luminanceImageSequence = zeros([size(luminanceFrame) numel(sceneOrOpticalImageSequence)]);
+                end
                 xAxis = squeeze(spatialSupport(1,:,1));
                 yAxis = squeeze(spatialSupport(:,1,2));
             end
         elseif (strcmp(sceneOrOpticalImage, 'optical image'))
             rgbFrame = oiGet(sceneOrOpticalImageSequence{iFrame}, 'rgb image');
-            luminanceFrame = sceneGet(sceneOrOpticalImageSequence{iFrame}, 'luminance');
+            if (showLuminanceMap)
+                luminanceFrame = sceneGet(sceneOrOpticalImageSequence{iFrame}, 'luminance');
+            end
             if (iFrame == 1)
                 spatialSupport = oiGet(sceneOrOpticalImageSequence{iFrame},'spatialsupport','microns');
                 rgbImageSequence = zeros([size(rgbFrame) numel(sceneOrOpticalImageSequence)]);
-                luminanceImageSequence = zeros([size(luminanceFrame) numel(sceneOrOpticalImageSequence)]);
+                if (showLuminanceMap)
+                    luminanceImageSequence = zeros([size(luminanceFrame) numel(sceneOrOpticalImageSequence)]);
+                end
                 xAxis = squeeze(spatialSupport(1,:,1));
                 yAxis = squeeze(spatialSupport(:,1,2));
             end
@@ -39,12 +43,28 @@ function visualizeSceneOrOpticalImageSequence(sceneOrOpticalImage, sceneOrOptica
         end
         
         rgbImageSequence(:,:,:,iFrame) = rgbFrame;
-        luminanceImageSequence(:,:,iFrame) = luminanceFrame;
+        if (showLuminanceMap)
+            luminanceImageSequence(:,:,iFrame) = luminanceFrame;
+        end
     end % iFrame
     
     % Determine ranges
-    rgbRange = [0 1]; % [min(rgbImageSequence(:)) max(rgbImageSequence(:))]
-    luminanceRange = mean(luminanceImageSequence(:)) * [0.9 1.1];
+    rgbRange = [0 1];
+    if (showLuminanceMap)
+        luminanceRange = mean(luminanceImageSequence(:)) * [0.9 1.1];
+    end
+    
+    hFig = figure(10); clf;
+    if (showLuminanceMap)
+        figSize = [10 10 950 475];
+    else
+        figSize = [10 10 500 475];
+    end
+    
+    set(hFig, 'Position', figSize, 'Color', [1 1 1]);
+    if (showLuminanceMap)
+        colormap(hot(1024));
+    end
     
     % Open video stream
     videoDir = colorGaborDetectVideosDir();
@@ -57,42 +77,51 @@ function visualizeSceneOrOpticalImageSequence(sceneOrOpticalImage, sceneOrOptica
     
     for iFrame = 1:numel(sceneOrOpticalImageSequence)
         % The RGB rendition of the scene/optical image
-        subplot('Position', [0.07 0.05 0.38 0.93]);
+        if (showLuminanceMap)
+            subplot('Position', [0.07 0.05 0.38 0.93]);
+        else
+            subplot('Position', [0.09 0.085 0.87 0.87]);
+        end
+        
         imagesc(xAxis, yAxis, squeeze(rgbImageSequence(:,:,:,iFrame)));
         axis 'image'
         set(gca, 'XLim', [xAxis(1) xAxis(end)], 'YLim', [yAxis(1) yAxis(end)], 'CLim', rgbRange, 'FontSize', 14);
         if (strcmp(sceneOrOpticalImage, 'scene'))
+            xlabel('space (world cm)', 'FontSize', 16, 'FontWeight', 'bold');
+            ylabel('space (world cm)', 'FontSize', 16, 'FontWeight', 'bold');
             title(sprintf('scene at t : %2.3f sec', timeAxis(iFrame)), 'FontSize', 16);
         else
+            xlabel('space (retinal microns)', 'FontSize', 14, 'FontWeight', 'bold');
+            ylabel('space (retinal microns)', 'FontSize', 14, 'FontWeight', 'bold');
             title(sprintf('optical image at t: %2.3f sec', timeAxis(iFrame)), 'FontSize', 16);
         end
-        xlabel('space (world cm)', 'FontSize', 16, 'FontWeight', 'bold');
-        ylabel('space (world cm)', 'FontSize', 16, 'FontWeight', 'bold');
         
-        % The luminance/illuminance map
-        subplot('Position', [0.52 0.05 0.38 0.93]);
-        imagesc(xAxis, yAxis, squeeze(luminanceImageSequence(:,:,iFrame)));
-        axis 'image'
-        set(gca, 'XLim', [xAxis(1) xAxis(end)], 'YLim', [yAxis(1) yAxis(end)], 'CLim', luminanceRange, 'FontSize', 14);
-        if (strcmp(sceneOrOpticalImage, 'scene'))
-            title(sprintf('scene luminance at t : %2.3f sec', timeAxis(iFrame)), 'FontSize', 16);
-        else
-            title(sprintf('retinal illuminance at t : %2.3f sec', timeAxis(iFrame)), 'FontSize', 16);
+        if (showLuminanceMap)
+            % The luminance/illuminance map
+            subplot('Position', [0.52 0.05 0.38 0.93]);
+            imagesc(xAxis, yAxis, squeeze(luminanceImageSequence(:,:,iFrame)));
+            axis 'image'
+            set(gca, 'XLim', [xAxis(1) xAxis(end)], 'YLim', [yAxis(1) yAxis(end)], 'CLim', luminanceRange, 'FontSize', 14);
+            if (strcmp(sceneOrOpticalImage, 'scene'))
+                xlabel('space (world cm)', 'FontSize', 16, 'FontWeight', 'bold');
+                title(sprintf('scene luminance at t : %2.3f sec', timeAxis(iFrame)), 'FontSize', 16);
+            else
+                xlabel('space (retinal microns)', 'FontSize', 14, 'FontWeight', 'bold');
+                title(sprintf('retinal illuminance at t : %2.3f sec', timeAxis(iFrame)), 'FontSize', 16);
+            end
+
+            % Add colorbar
+            originalPosition = get(gca, 'position');
+            hCbar = colorbar('eastoutside', 'peer', gca); % , 'Ticks', cbarStruct.ticks, 'TickLabels', cbarStruct.tickLabels);
+            hCbar.Orientation = 'vertical'; 
+            hCbar.Label.String = 'cd/m2'; 
+            hCbar.FontSize = 14; 
+            hCbar.FontName = 'Menlo'; 
+            hCbar.Color = [0.2 0.2 0.2];
+            % The addition changes the figure size, so undo this change
+            newPosition = get(gca, 'position');
+            set(gca,'position',[newPosition(1) newPosition(2) originalPosition(3) originalPosition(4)]);
         end
-        xlabel('space (retinal microns)', 'FontSize', 14, 'FontWeight', 'bold');
-        ylabel('space (retinal microns)', 'FontSize', 14, 'FontWeight', 'bold');
-        
-        % Add colorbar
-        originalPosition = get(gca, 'position');
-        hCbar = colorbar('eastoutside', 'peer', gca); % , 'Ticks', cbarStruct.ticks, 'TickLabels', cbarStruct.tickLabels);
-        hCbar.Orientation = 'vertical'; 
-        hCbar.Label.String = 'cd/m2'; 
-        hCbar.FontSize = 14; 
-        hCbar.FontName = 'Menlo'; 
-        hCbar.Color = [0.2 0.2 0.2];
-        % The addition changes the figure size, so undo this change
-        newPosition = get(gca, 'position');
-        set(gca,'position',[newPosition(1) newPosition(2) originalPosition(3) originalPosition(4)]);
         
         % Render
         drawnow
